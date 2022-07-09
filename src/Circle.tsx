65a5df1e4977af0fbf3bb24e59170a166cc6a950
wkdrhkdwls <79461880+wkdrhkdwls@users.noreply.github.com>
2022-07-09 21:22:22 +0900

    220709-before reactquery

---
+++ src/Circle.tsx
@@ -0,0 +1,34 @@
+import { useState } from "react";
+import styled from "styled-components";
+
+interface ContainerProps {
+  bgColor: string;
+  borderColor: string;
+}
+
+interface CircleProps {
+  bgColor: string;
+  borderColor?: string;
+  text?: string;
+}
+
+const Container = styled.div<ContainerProps>`
+  width: 200px;
+  height: 200px;
+  background-color: ${(props) => props.bgColor};
+  border-radius: 100px;
+  border: 3px solid ${(props) => props.borderColor};
+`;
+
+function Circle({ bgColor, borderColor, text = "default text" }: CircleProps) {
+  const [value, setValue] = useState<number | string>(0);
+  setValue(2);
+  setValue("hi");
+  return (
+    <Container bgColor={bgColor} borderColor={borderColor ?? bgColor}>
+      {text}
+    </Container>
+  );
+}
+
+export default Circle;
